fcd4c7c7e6772c8e475a40150766020558f6753d
lebang <lebang271206@gmail.com>
2026-03-21 13:53:39 +0700

    lab6

--- src/app/pages/stories/stories.ts
+++ src/app/pages/stories/stories.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Story {
+  id: number;
+  title: string;
+  author: string;
+  views: number;
+}
 
 @Component({
   selector: 'app-stories',
@@ -6,42 +14,51 @@ import { Component } from '@angular/core';
   templateUrl: './stories.html',
   styleUrl: './stories.css',
 })
-export class Stories {
-  stories = [
-    {
-      title: 'One Piece',
-      author: 'Oda',
-      views: 100000,
-      year: 1997,
-      genre: "Phiêu lưu",
-      image: "https://wallpapers.com/images/featured-full/one-piece-pictures-bjm9tdff9yzguoup.jpg",
-    },
-    {
-      title: 'Naruto',
-      author: 'Kishimoto',
-      views: 90000,
-      year: 1999,
-      genre: "Hành động",
-      image: "https://easydrawingguides.com/wp-content/uploads/2017/05/how-to-draw-naruto-featured-image-1200.png",
-    },
-    {
-      title: 'Doraemon',
-      author: 'Fujiko F Fujio',
-      views: 70000,
-      year: 1969,
-      genre: "Hài kịch",
-      image: "https://scontent.fhan12-1.fna.fbcdn.net/v/t39.30808-6/300786145_451257823693689_6259289295520386259_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=1d70fc&_nc_eui2=AeHLynwMHjqYKSjPyP7t9AeX8WlMGkt_s4rxaUwaS3-zinpgzItmnMZlVpT1jAgEXtimoFQJKcprDWXSU8dkXy0X&_nc_ohc=ec2bKZq1dTIQ7kNvwFxDtaT&_nc_oc=AdmyP9xwZFEvfA5qQdQa8N5iR6Q6aU6i9oOEIi97_u5jv65Lg5Eev91sW7kclWRIOOg&_nc_zt=23&_nc_ht=scontent.fhan12-1.fna&_nc_gid=c-eDrgejWaERLL4du0Z6Rg&_nc_ss=8&oh=00_Afzthk9NhQW97wXoTQIiusY6jQ7Fu3p3kFd4UfTJmanjXw&oe=69BAB82A",
-    },
-    {
-      title: 'Hunter x Hunter',
-      author: 'Togashi',
-      views: 120000,
-      year: 1998,
-      genre: "Phiêu lưu",
-      image: "https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=cover,format=auto,quality=85,width=1920/keyart/GY3VKX1MR-backdrop_wide",
-    },
-  ];
-  handleClick(title: string) {
-    alert(`Bạn đã click vào truyện "${title}"`);
+export class Stories implements OnInit {
+  stories: Story[] = [];
+
+  loading = false;
+  error = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getStories();
+  }
+
+  getStories() {
+    this.loading = true;
+    this.error = '';
+
+    console.log('CALL API');
+
+    this.http.get<Story[]>('http://localhost:3000/stories').subscribe({
+      next: (data) => {
+        console.log('SUCCESS:', data);
+        this.loading = false;
+        this.stories = data;
+      },
+      error: () => {
+
+         console.log('ERROR:', this.error);
+        this.loading = false;
+        this.error = 'Không thể tải dữ liệu';
+      },
+    });
+  }
+
+  deleteStory(id: number) {
+    const confirmDelete = confirm('Bạn có chắc muốn xóa không?');
+    if (!confirmDelete) return;
+
+    this.http.delete(`http://localhost:3000/stories/${id}`).subscribe({
+      next: () => {
+        this.stories = this.stories.filter((story) => story.id !== id);
+        alert('Xóa thành công');
+      },
+      error: () => {
+        alert('Xóa thất bại');
+      },
+    });
   }
 }
